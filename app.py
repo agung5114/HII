c9745ab9f383cb6207119f3ac74c4cc09cbdbc2f
agung5114 <43102810+agung5114@users.noreply.github.com>
2023-09-19 21:04:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -111,10 +111,10 @@ elif choice == "Geospatial Factors & Risk Mapping":
         k1,k2 = st.columns((1,4))
         with k1:
             if datapeserta['JK'].values[0] ==0:
-                st.image(Image.open('cew.jpeg'))
+                st.image('cew.jpeg')
                 # st.image(Image.open('accnt.png'))
             else:
-                st.image(Image.open('cow.jpeg'))
+                st.image('cow.jpeg')
             with st.expander('Data Pokok Peserta', expanded=True):
                 st.write(f"Nomor Peserta: {datapeserta['PSTV01'].values[0]}")
                 i2 = datapeserta['JK'].values[0]
