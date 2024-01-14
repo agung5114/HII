5500e8645d40bf79fa8af25dddb9ffdf0680dfd8
agung5114 <43102810+agung5114@users.noreply.github.com>
2024-01-15 00:26:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def getExcel(url):
 genre = st.sidebar.radio("Sub menu",('Timeline by Regions','Region Characteristics and Risk Level','Region Resource Availability Level','Patient Risk Profile'))
 if genre == 'Timeline by Regions':
     components.html('''
-                    <div style="position: relative; width: 100%; height: 0; padding-top: 26.2500%;
+                    <div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%;
 padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 0.6em; margin-bottom: 0.9em; overflow: hidden;
 border-radius: 8px; will-change: transform;">
 <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
